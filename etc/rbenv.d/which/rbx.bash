f1a74f6b53bbcb4ed26c1ec73a80cc81556b1d95
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-07-07 17:47:49 +0200

    fix discovery of gem binstubs
    Previously, Rubinius 2 installed binstubs to:
    
      $(rbenv prefix)/{1.8,1.9}/bin
    
    Now it seems that has changed to:
    
      $(rbenv prefix)/gems/{1.8,1.9,2.0}/bin
    
    The tricky thing is figuring out the currently selected Rubinius mode.
    Very crude detection is done on the `RBXOPT=-X...` value.

--- etc/rbenv.d/which/rbx.bash
+++ etc/rbenv.d/which/rbx.bash
@@ -1,3 +1,9 @@
-if [ ! -x "$RBENV_COMMAND_PATH" ] && [[ $RBENV_VERSION =~ rbx-2 ]]; then
-  export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/1.9/bin/${RBENV_COMMAND}"
+if [ ! -x "$RBENV_COMMAND_PATH" ] && [[ $RBENV_VERSION == "rbx-2"* ]]; then
+  if [[ $RBXOPT == "-X"* ]]; then
+    rbx_mode="${RBXOPT:2:4}"
+  else
+    # FIXME: this can vary depending on the default mode that Rubinius was compiled with!
+    rbx_mode=1.9
+  fi
+  export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/gems/${rbx_mode}/bin/${RBENV_COMMAND}"
 fi
